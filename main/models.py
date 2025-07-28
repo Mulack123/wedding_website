72b55abd9085fe03079c4d45124b8059dc8840a5
Ben Coleman <benjamin.coleman@me.com>
2025-07-28 22:14:59 +0100

    Init model and example query
    - Added a model for guest and rsvp
    - Examples of how to query and use in page, needs cleanup
    - Registered Guest Model for use in admin app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Guest(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+
+class RSVP(models.Model):
+    guest = models.OneToOneField(Guest, on_delete=models.CASCADE, primary_key=True)
+    going = models.BooleanField()
+    # TODO @calum: Add a timestamp
+    # TODO: food choice
+
+# TODO: Decide on structure for Music suggestions
+# class Music(models.Model):
+#     guest = models.ForeignKey(Guest, on_delete="cascade")
+#     title = models.CharField(max_length=100)
+#     artist = models.CharField(max_length=100)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
+
+from main.models import Guest
 
 # Create your views here.
 def home(request):
-    return render(request, "main/index.html", {"title":"I&C Wedding"})
+    guests = Guest.objects.all()
+    return render(request, "main/index.html", {
+        "title":"I&C Wedding",
+        "guests":guests,
+    })
