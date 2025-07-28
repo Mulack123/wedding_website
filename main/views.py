f863610c4a48f2f127a182d08a57af583fa89e6c
Ben Coleman <benjamin.coleman@me.com>
2025-07-28 21:20:17 +0100

    Boilerplate for templates and url routing

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+def home(request):
+    template = loader.get_template("main/index.html")
+    return render(request, "main/index.html", {"name":"Calum"})
