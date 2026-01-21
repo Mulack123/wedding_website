73aa7c1f349adf9e66cb14f55e8fe4de81e25808
Calum Keating <calumktng@gmail.com>
2026-01-21 20:12:04 +0000

    Added unit test for Music model

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from .models import Music, Invite
 
 # Create your tests here.
+class MusicModelTest(TestCase):
+    def setUp(self):
+        self.invite = Invite.objects.create(name="Calum", code="test-code")
+    def test_spotify_link_blank(self):
+        music = Music.objects.create(
+            invite=self.invite,
+            title="test_title",
+            artist="test_artist"
+        )
+        self.assertEqual(music.spotify_link, "")
