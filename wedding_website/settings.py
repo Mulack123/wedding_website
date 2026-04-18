6b5a463c34c3d3ae39727533e645c86c399794d9
Calum Keating <calumktng@gmail.com>
2026-04-18 01:07:27 +0100

    Add CSRF_TRUSTED_ORIGINS setting for Railway deployment

--- wedding_website/settings.py
+++ wedding_website/settings.py
@@ -9,6 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-avhx1gf*5%3f)9fh_()in
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost').split()
 
 INSTALLED_APPS = [
     'main',
