b4f64f0152f2429dc11acb4f5b3132f3dfdf0857
Calum Keating <calumktng@gmail.com>
2026-04-18 01:09:29 +0100

    Trust Railway proxy SSL header to fix CSRF verification

--- wedding_website/settings.py
+++ wedding_website/settings.py
@@ -10,6 +10,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost').split()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 INSTALLED_APPS = [
     'main',
